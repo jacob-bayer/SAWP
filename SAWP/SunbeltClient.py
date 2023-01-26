76e7fa3e5e8e61d39dea6c346c87e61f07eeed66
Jacob Bayer <jacobbenjaminbayer@gmail.com>
2023-01-26 13:48:46 -0500

    clean up

--- SAWP/SunbeltClient.py
+++ SAWP/SunbeltClient.py
@@ -5,12 +5,11 @@ from .generators import generators
 
 class SunbeltClient(SunbeltClientBase):
     
-    def __init__(self, env = 'dev'):
-        hosts = {'local': "http://127.0.0.1:5000/graphql",
-                 'dev_gcloud' : 'https://sunbeltapi.uc.r.appspot.com/graphql',
-                 'dev' : 'https://sunbelt.herokuapp.com/graphql',
-                  'prod' : ''}
-        self.host = hosts[env]
+    def __init__(self, server = 'heroku'):
+        servers = {'local': "http://127.0.0.1:5000/graphql",
+                 'heroku' : 'https://sunbelt.herokuapp.com/graphql',
+                 'prod' : ''}
+        self.host = hosts[server]
         super().__init__(self.host)
     
 
@@ -31,11 +30,11 @@ class SunbeltClient(SunbeltClientBase):
         return generators.SubredditGenerator(self)
 
     @property
-    def post_details(self):#, sun_post_id = None):
-        return generators.PostDetailGenerator(self)#, sun_post_id = sun_post_id)
+    def post_details(self):
+        return generators.PostDetailGenerator(self)
 
     @property
-    def comment_details(self):#, sun_comment_id = None):
-        return generators.CommentDetailGenerator(self)#, sun_comment_id = sun_comment_id)
+    def comment_details(self):
+        return generators.CommentDetailGenerator(self)
 
         
